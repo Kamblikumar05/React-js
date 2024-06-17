c8b7b5d02fdb296227bde46cd29b95b85f026010
main <kamblikumar05@gmail.com>
2024-06-17 19:10:16 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+const heading = React.createElement("h1", {id:"heading"}, "Hello world From React js");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); 
+/* 
+<div id="parent">
+    <div id="child1">
+        <h1>Hello world<h1>
+        <h3>Hello world<h3>
+    </div>
+    <div id="child2">
+        <h1>Hello world<h1>
+        <h3>Hello world<h3>
+    </div>
+</div>
+*/
+const parent = React.createElement("div",{id:"parent"}, [
+    React.createElement("div",{id:"child1"},[ React.createElement("h1",{ },"Hello World"), React.createElement("h3",{ },"Hello World 2")]),React.createElement("div",{id:"child2"},[ React.createElement("h1",{ },"Hello World"), React.createElement("h3",{ },"Hello World 2")])] );
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
+root1.render(parent)
